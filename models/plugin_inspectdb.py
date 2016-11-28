40f0ee588248c9616a470a11e11e15e2b4b9f80b
Manuele Pesenti <manuele@inventati.org>
2016-11-28 23:26:45 +0100

    added services

--- models/plugin_inspectdb.py
+++ models/plugin_inspectdb.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import json
+
 def _plugin_inspectdb():
 
     from plugin_inspectdb import InspectDB, loopOconns
@@ -31,6 +33,62 @@ def _plugin_inspectdb():
     return odbs
 
 odbs = _plugin_inspectdb()
+
+class DBService(object):
+    """ """
+
+    @staticmethod
+    def _cast(f, v):
+        """
+        f @Field  : object;
+        v @string : value to be inserted
+        """
+        if f.type in (None, 'string', 'text'):
+            return v
+        elif f.type == 'integer':
+            return int(v)
+        elif f.type == 'boolean':
+            return bool(v)
+        elif f.type == 'double':
+            return float(v)
+        elif f.type == 'date':
+            return  dateutil.parser.parse(v).date()
+        elif f.type == 'datetime':
+            return dateutil.parser.parse(v)
+        elif f.type == 'json':
+            return json.loads(v) if isinstance(v, basestring) else v
+        else:
+            raise NotImplementedError()
+
+    @classmethod
+    def insert(cls, dbname, tablename, **kw):
+        """ """
+        @auth.requires_login()
+        def _main():
+            tab = odbs[dbname][tablename]
+            return tab.insert(**{k: cls._cast(tab[k], v) for k,v in kw.iteritems()})
+        return _main()
+
+    @classmethod
+    def bulk_insert(cls, dbname, tablename, _data):
+        """
+        data @list : list of dictionaries
+        """
+        data = json.loads(_data)
+        @auth.requires_login()
+        def _main():
+            return odbs[dbname][tablename].bulk_insert(map(lambda kw: {k: cls._cast(tab[k], v) for k,v in kw.iteritems()}, data))
+        return _main()
+
+@service.json
+def db_insert(dbname, tablename, **kw):
+    """ """
+    return dict(id = DBService.insert(dbname, tablename, **kw))
+
+@service.json
+def db_bulk_insert(dbname, tablename, data):
+    """ """
+    return dict(ids = DBService.bulk_insert(dbname, tablename, data))
 
 response.menu += [
     (STRONG(SPAN(_class="glyphicon glyphicon-sunglasses", **{"_aria-hidden": "true"}), " ", T("Inspect dbs"), _style="color: yellow;"), False, "#", [
